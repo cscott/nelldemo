0854497fe4c5d12dcff7e664abd82d2092c9da69
C. Scott Ananian <cscott@cscott.net>
2011-10-28 18:55:22 -0400

    Factor out KS string synthesis.

---
+++ ks.js
@@ -0,0 +1,33 @@
+function KS(sampleRate, freq) {
+  this.reset(sampleRate, freq);
+}
+KS.prototype = {
+  sampleRate: 1,
+  bufferPos: 0,
+  bufferLen: 0,
+  buffer: null,
+  sample: 0,
+  plucked: false,
+
+  generate: function() {
+    var next = this.buffer[this.bufferPos];
+    this.buffer[this.bufferPos++] = (next + this.sample) / 2;
+    this.sample = next;
+    if (this.bufferPos >= this.bufferLen) {
+      this.bufferPos = 0;
+    }
+  },
+  getMix: function() {
+    return this.sample;
+  },
+  reset: function(sampleRate, freq) {
+    this.sampleRate = sampleRate || this.sampleRate;
+    this.bufferLen = freq ? Math.round((this.sampleRate/freq)-.5) : 100;
+    this.buffer = new Float32Array(this.bufferLen);
+    for (var i=0; i<this.bufferLen; i++) {
+      this.buffer[i] = freq ? Math.random() : 0;
+    }
+    this.bufferPos = 0;
+    this.plucked = false;
+  }
+}
